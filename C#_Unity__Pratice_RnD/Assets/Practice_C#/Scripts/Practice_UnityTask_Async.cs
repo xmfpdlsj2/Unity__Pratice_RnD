27d6b94d242553798724672861364bd3bc4393b9
HoJun Sung <sunghojn@naver.com>
2023-05-10 20:15:55 +0900

    Add simple Unitask code
    Compair Unitaskvoid and async

--- C#_Unity__Pratice_RnD/Assets/Practice_C#/Scripts/Practice_UnityTask_Async.cs
+++ C#_Unity__Pratice_RnD/Assets/Practice_C#/Scripts/Practice_UnityTask_Async.cs
@@ -10,9 +10,15 @@ public class Practice_UnityTask_Async : MonoBehaviour
     {
         
     }
-    private void Start()
+    private async void Start()
     {
-        
+        Debug.Log("0");
+        var a = Delay1();
+        await a;
+        Debug.Log("1");
+
+        Delay1Async().Forget();
+        Debug.Log("3");
     }
 
     private static UniTask Delay1()
@@ -23,7 +29,9 @@ public class Practice_UnityTask_Async : MonoBehaviour
 
     private static async UniTask Delay1Async()
     {
+        Debug.Log("4");
         await UniTask.Delay(1000);
+        Debug.Log("5");
         await UniTask.Delay(1000);
         Delay1Void().Forget();
     }
@@ -31,6 +39,7 @@ public class Practice_UnityTask_Async : MonoBehaviour
     private static async UniTaskVoid Delay1Void()
     {
         await UniTask.Delay(1000);
+        Debug.Log("6");
     }
 }
 
